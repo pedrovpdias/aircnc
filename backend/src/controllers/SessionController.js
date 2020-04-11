127ff5bc5dd264b2b92fd926cb99e5adc2c98f47
Pedro Dias <pedrovpdias@gmail.com>
2020-04-11 18:35:16 -0300

    Add backend folder

---
+++ backend/src/controllers/SessionController.js
@@ -0,0 +1,17 @@
+const User = require('../models/Users');
+ 
+module.exports = {
+    async store( request, response) {
+        const { email } = request.body;
+
+        let user = await User.findOne({ email });
+
+        if (!user){
+            user = await User.create({
+                email
+            });
+        }
+
+        return response.json(user);
+    }
+};
---
+++ backend/src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://dev_root:00940421@devclusterpvpd-dzi5a.gcp.mongodb.net/aircnc?authSource=admin&replicaSet=DevClusterPVPD-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true', {
+   useNewUrlParser: true,
+   useUnifiedTopology: true,
+})
+
+app.use(cors());
+app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
+app.use(routes);
+
+app.listen(3333);
